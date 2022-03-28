6996bc92b938c2284845692e650e163dd7636a14
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2022-03-28 15:58:31 -0300

    ProjectById

--- frontend/src/components/layout/ProjectCard/ProjectCard.jsx
+++ frontend/src/components/layout/ProjectCard/ProjectCard.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import "./ProjectCard.css"
+
 function ProjectCard({project}) {
     return ( 
         <>
-            <div className="card h-100" >
+            <div className="card card-project h-100 mt-5" >
                 <img src={project.image} className="card-img-top" alt={project.name} />
                 <div className="card-body">
-                    <h5 className="card-title">{project.name}</h5>
+                    <h5 className="card-title text-center">{project.name}</h5>
                     <p className="card-text">{project.description}</p>
                 </div>
                 <div className="card-footer">
                     <Link to={`/project/${project.id}`}>
-                        <div className="btn btn-outline-success w-100">Ver Mais</div>
+                        <div className="btn btn-outline-primary w-100">Ver Mais</div>
                     </Link>
                 </div>
             </div>
--- frontend/src/components/layout/Projects/Projects.jsx
+++ frontend/src/components/layout/Projects/Projects.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import ProjectCard from "../ProjectCard/ProjectCard";
 function Projects() {
-  const project = {
-    id:1,  
-    image:
-      "https://github.com/DouglasSilvaPrado/Sistema-de-Fastfood/raw/main/SistemaDeVendas/src/imagens/TelaPrincipal.JPG",
-    name: "Sistema de Fastfood",
-    description: "Sistema desenvolvido em Java",
-    type: "Desktop",
-    code: "https://github.com/DouglasSilvaPrado/Sistema-de-Fastfood",
-    tools: "Java",
-    demo: "https://www.linkedin.com/feed/update/urn:li:activity:6891150094536630272/",
-  };
+    const project = {
+        id:1,  
+        image:
+          "https://github.com/DouglasSilvaPrado/Sistema-de-Fastfood/raw/main/SistemaDeVendas/src/imagens/TelaPrincipal.JPG",
+        gif:"https://i.ibb.co/cCDjWL0/Sistema-Fast-Food.gif",  
+        name: "Sistema de Fastfood",
+        description: "Sistema desenvolvido em Java",
+        type: "Desktop",
+        code: "https://github.com/DouglasSilvaPrado/Sistema-de-Fastfood",
+        tools: "Java",
+        demo: "https://www.linkedin.com/feed/update/urn:li:activity:6891150094536630272/",
+      };
 
   return (
     <>
@@ -20,11 +21,27 @@ function Projects() {
           <h2>Projetos</h2>
           <h3 className="text-base">Meus Projetos Pessoais e Profissionais</h3>
         </div>
-        <div>
-          <div key={project.id} className="col col-sm-6 col-lg-4 col-xl-3 mb-3">
+
+        <div className="row">
+          <div key={project.id} className="col-md-6 col-lg-4 mb-3">
             <ProjectCard project={project} />
           </div>
+          <div key={project.id} className="col-md-6 col-lg-4 mb-3">
+            <ProjectCard project={project} />
+          </div>
+          <div key={project.id} className="col-md-6 col-lg-4 mb-3">
+            <ProjectCard project={project} />
+          </div>
+          <div key={project.id} className="col-md-6 col-lg-4 mb-3">
+            <ProjectCard project={project} />
+          </div>
+          <div key={project.id} className="col-sm-12 col-md-6 col-lg-4 mb-3">
+            <ProjectCard project={project} />
+          </div>
+          
+          
         </div>
+
       </div>
     </>
   );
