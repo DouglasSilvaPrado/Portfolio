bc19690dd981127994d27e685a8c257ba7c840f2
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2022-04-11 16:37:30 -0300

    fake-server

--- frontend/src/components/utils/Request.jsx
+++ frontend/src/components/utils/Request.jsx
@@ -1,2 +1,3 @@
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "https://portfolio-api-backend.herokuapp.com"
-// export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:5000"
+// export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "https://portfolio-api-backend.herokuapp.com"
+//export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:5000"
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "https://fake-server-port.herokuapp.com"
