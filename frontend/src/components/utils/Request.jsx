58878a1ea503a7342cea24dd05650d65c08443c3
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2023-03-10 17:45:56 -0300

    fix: change api url

--- frontend/src/components/utils/Request.jsx
+++ frontend/src/components/utils/Request.jsx
@@ -1,3 +1,3 @@
 // export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "https://portfolio-api-backend.herokuapp.com"
 //export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:5000"
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "https://fake-server-port.herokuapp.com"
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "https://api-portfolio-three.vercel.app"
